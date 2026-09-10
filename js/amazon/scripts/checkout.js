b43214e77525e72a1af3e673def38e2c93d1a176
lopxans <lopxans@gmail.com>
2026-09-10 15:04:32 +0545

    Delete products form HTML when clicking delete

--- js/amazon/scripts/checkout.js
+++ js/amazon/scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -79,13 +79,17 @@ cart.forEach((cartItem) => {
 
     `;
 });
-console.log(cartSummaryHTML);
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+
+        const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+        );
+        container.remove();
     })
 });
