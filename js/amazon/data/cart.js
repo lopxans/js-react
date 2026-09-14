ca99d405bb6019f0500bd002738889ebc516fb33
lopxans <lopxans@gmail.com>
2026-09-14 14:29:30 +0545

    error fixt

--- js/amazon/data/cart.js
+++ js/amazon/data/cart.js
@@ -5,16 +5,16 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
-      deliveryOptionId: "1",
+      deliveryOptionId: '1'
     },
-
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
-      deliveryOptionId: "2",
+      deliveryOptionId: '2'
     },
   ];
 }
+
 
 export function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -35,12 +35,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-
-      // FIX:
-      // "deliveryoptionId" was incorrect.
-      // JavaScript is case-sensitive.
-      // Changed it to "deliveryOptionId".
-      deliveryOptionId: "1",
+      deliveryoptionId: '1'
     });
   }
 
@@ -57,9 +52,9 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
-
   saveToStorage();
 }
+
 
 export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
@@ -71,6 +66,5 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   });
 
   matchingItem.deliveryOptionId = deliveryOptionId;
-
   saveToStorage();
 }
